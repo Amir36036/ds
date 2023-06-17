e3e909c95903bb5cf988ce3879f42382824f5fc3
amir shaikh <amir@gmail.com>
2023-06-17 22:57:18 +0530

    find first and last index of given key using binary seach

---
+++ src/main/java/org/ds/array/FindFirstAndLastPosition.java
@@ -0,0 +1,57 @@
+package org.ds.array;
+
+public class FindFirstAndLastPosition {
+
+    public static int findFirstPosition(int[] arr, int size, int key){
+
+        int startIndex = 0;
+        int endIndex = size - 1;
+        int midIndex = startIndex + (endIndex - startIndex)/2;
+        int ans = -1;
+        while (startIndex <= endIndex){
+
+            if(key == arr[midIndex]){
+                ans = midIndex;
+                endIndex = midIndex - 1;
+            }else if (key > arr[midIndex]){
+                startIndex = midIndex + 1;
+            }else {
+                endIndex = midIndex - 1;
+            }
+
+            midIndex = startIndex + (endIndex - startIndex)/2;
+        }
+        return ans;
+    }
+
+    public static int findLastPosition(int[] arr, int size, int key){
+
+        int startIndex = 0;
+        int endIndex = size - 1;
+        int midIndex = startIndex + (endIndex - startIndex)/2;
+        int ans = -1;
+        while (startIndex <= endIndex){
+
+            if(key == arr[midIndex]){
+                ans = midIndex;
+                startIndex = midIndex + 1;
+            }else if (key > arr[midIndex]){
+                startIndex = midIndex + 1;
+            }else {
+                endIndex = midIndex - 1;
+            }
+
+            midIndex = startIndex + (endIndex - startIndex)/2;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,6,6,6,6,6,14};
+        int firstIndex6 = findFirstPosition(arr, 7, 6);
+        System.out.println("first index of 6 : " +firstIndex6);
+
+        int lastIndex6 = findLastPosition(arr, 7, 6);
+        System.out.println("last index of 6 : " +lastIndex6);
+    }
+}
